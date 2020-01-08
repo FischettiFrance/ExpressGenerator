da084a9821b76529657446c404dfd50cb1e3243f
FischettiFrance <fischetti.francesco@iisgalvanimi.edu.it>
2020-01-08 18:17:22 +0000

    ultimate

---
+++ FAKER_JS/routes/users.js
@@ -0,0 +1,39 @@
+var express = require('express');
+var router = express.Router();
+var faker = require('faker');
+const fs = require('fs');
+
+/* GET users listing. */
+router.get('/', function(req, res, next) {
+ res.send(createFakePerson());
+});
+
+router.get('/generate', function (req, res, next) {
+    let vettore = { people: [] };
+    for (let index = 0; index < 10; index++) {
+        vettore.people.push(createFakePerson());
+    }
+
+
+
+    let data = JSON.stringify(vettore);
+    fs.writeFileSync('people.json', data);
+
+    res.send("utenti inseriti");
+});
+
+function createFakePerson()
+{
+ let randomName = faker.name.findName(); // Rowan Nikolaus
+ let randomEmail = faker.internet.email(); // Kassandra.Haley@erich.biz
+ let randomCard = faker.helpers.createCard(); // random contact card containing many properties
+
+ let person = {
+   name:randomName,
+   email:randomEmail,
+   card: randomCard
+ }
+ return person;
+}
+
+module.exports = router;
